93b626cdea61312bca79f31dfe88b8f4e161b684
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-02-04 22:09:51 +0400

    particle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ParticlesModule } from 'angular-particle';
+
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
