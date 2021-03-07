9771fdf55e0a49c8835c18a56b02b1fb985baa26
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-03-07 22:22:51 +0400

    add

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,47 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  // Imagedata = ["../../assets/contact.jpg", "../../assets/footer.png"];
-
-  // startIndex = 1;
-
   constructor() { }
 
   backToTop(){
     window.scrollTo({top: 0, behavior: "smooth"});
   }
 
-  // __FunctionSlide() {
-  //   const slides = Array.from(document.getElementsByClassName('mall-show-slide'));
-  //   if (slides === []) {
-  //     this.Repeat();
-  //   }
-  //   for (const x of slides) {
-  //     const y = x as HTMLElement;
-  //     y.style.display = 'none';
-  //   }
-  //   if (this.startIndex > slides.length - 1) {
-  //     this.startIndex = 0;
-  //     const slide = slides[this.startIndex] as HTMLElement;
-  //     slide.style.display = 'block';
-  //     this.startIndex++;
-  //   } else {
-
-  //     const slide = slides[this.startIndex] as HTMLElement;
-  //     slide.style.display = 'block';
-  //     this.startIndex++;
-  //   }
-  // }
-
-  // Repeat() {
-  //   setTimeout(() => {
-  //     this.__FunctionSlide();
-  //     this.Repeat();
-  //   }, 2000);
-  // }
-
   ngOnInit(): void {
-    // this.Repeat();
+
   }
 }
 
