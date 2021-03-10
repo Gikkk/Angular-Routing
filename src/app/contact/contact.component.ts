24076673039dab5c202c7953c8bb046362749a01
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-03-10 23:47:53 +0400

    add

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, AfterViewInit, Renderer2, ViewChild } from '@angular/core';
 import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,12 @@ import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/for
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
-
+export class ContactComponent implements AfterViewInit, OnInit{
+  availability = 'Available'
   signUpForm: FormGroup;
+  @ViewChild("status") onlineStatus: ElementRef;
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient, private renderer: Renderer2,) { }
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
@@ -23,6 +24,10 @@ export class ContactComponent implements OnInit {
 
   }
 
+  ngAfterViewInit(){
+    this.getTime();
+  }
+
   onSubmit(){
     this.http.post<{name: string, email: EmailValidator, subject: string, message: string}>('https://giorgi-zho-default-rtdb.europe-west1.firebasedatabase.app/messages.json',
      this.signUpForm.value
@@ -30,5 +35,17 @@ export class ContactComponent implements OnInit {
       console.log(data);
       this.signUpForm.reset();
     });
+  }
+
+  getTime(){
+    let currentTime = new Date();
+    let hour = currentTime.getHours()
+
+    if (hour >= 9 && hour <= 16 ) {
+      this.renderer.setStyle(this.onlineStatus.nativeElement, 'backgroundColor', 'rgb(28, 221, 28)');
+    } else {
+      this.renderer.setStyle(this.onlineStatus.nativeElement, 'backgroundColor', 'red');
+      this.availability = 'Not available';
+    }
   }
 }
