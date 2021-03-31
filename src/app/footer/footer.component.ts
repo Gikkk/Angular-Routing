c08711e7cf9b398a488e7484460b1a43ded735d8
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-03-31 21:12:51 +0400

    add

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,10 +7,6 @@ import { ScrollService } from  '../scroll.service';
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  animations: [trigger("fade", [
-  state("void", style({ opacity: 0 })),
-  transition("void <=> *", [animate("0.5s ease-in-out")])
-  ])]
 })
 export class FooterComponent implements OnInit{
 
@@ -23,66 +19,4 @@ export class FooterComponent implements OnInit{
   backToTop(){
     this.scrollService.scrollToTop();
   }
-
-  counter = 0;
-  slideItems = [
-    { src: 'https://placeimg.com/600/600/any', title: 'Title 1' },
-    { src: 'https://placeimg.com/600/600/nature', title: 'Title 2' },
-    { src: 'https://placeimg.com/600/600/sepia', title: 'Title 3' },
-    { src: 'https://placeimg.com/600/600/people', title: 'Title 4' },
-    { src: 'https://placeimg.com/600/600/tech', title: 'Title 5' }
-  ];
-
-  showNextImage() {
-    if (this.counter < this.slideItems.length -1) {
-      this.counter += 1;
-    }
-  }
-
-  showPreviousImage() {
-    if (this.counter >= 1) {
-      this.counter = this.counter - 1;
-    }
-  }
 }
-
-// this.navLinks.forEach((navLink, activeIndex) => {
-//   navLink.addEventListener("click", () => {
-//     // nav-link
-//     this.navLinks.forEach(navLink => navLink.classList.remove("active"));
-//     navLink.classList.add("active");
-
-//     // slide
-//     let currentSlide = document.querySelector(".active");
-//     let slideFadeOut = currentSlide.animate(
-//       [
-//         { transform: "translateX(0)", opacity: 1 },
-//         { transform: "translateX(5%)", opacity: 0 }
-//       ],
-//       {
-//         duration: 600,
-//         easing: "ease-in",
-//         fill: "forwards"
-//       }
-//     );
-
-//     slideFadeOut.onfinish = () => {
-//       this.slides.forEach(slide => slide.classList.remove("active"));
-//       let activeSlide = this.slides[activeIndex];
-//       activeSlide.classList.add("active");
-//       activeSlide.animate(
-//         [
-//           {
-//             transform: "translateX(-5%)",
-//             opacity: 0
-//           },
-//           {
-//             transform: "translateX(0)",
-//             opacity: 1
-//           }
-//         ],
-//         { duration: 600, easing: "ease-out", fill: "forwards" }
-//       );
-//     };
-//   });
-// });
