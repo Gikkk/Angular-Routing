ea08158a0f42ec23e02b7c4261353f884f3e13b8
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-04-01 21:31:54 +0400

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { ParticlesModule } from 'angular-particle';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -12,13 +11,13 @@ import { ContactComponent } from './contact/contact.component';
 import { MainComponent } from './main/main.component';
 import { MapComponent } from './contact/map/map.component';
 import { AboutComponent } from './about/about.component';
-import { SliderComponent } from './about/slider/slider.component';
 import { ProjectComponent } from './project/project.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CircularMenuComponent } from './main/circular-menu/circular-menu.component';
 import { TechStackComponent } from './tech-stack/tech-stack.component';
 import { FocusComponent } from './focus/focus.component';
+import { CardComponent } from './about/card/card.component';
 
 const appRoutes: Routes = [
   {path: '', component: MainComponent},
@@ -36,13 +35,13 @@ const appRoutes: Routes = [
     MainComponent,
     MapComponent,
     AboutComponent,
-    SliderComponent,
     ProjectComponent,
     HeaderComponent,
     FooterComponent,
     CircularMenuComponent,
     TechStackComponent,
-    FocusComponent
+    FocusComponent,
+    CardComponent
 ],
   imports: [
     BrowserModule,
