d4cc8a6a04e71c411517187fd62acbd8e8c61e32
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-04-01 13:43:21 +0400

    add

--- src/app/about/slider/slider.component.ts
+++ src/app/about/slider/slider.component.ts
@@ -9,40 +9,8 @@ export class SliderComponent implements OnInit {
 
   constructor() { }
 
-  Imagedata = ["assets/slider/myPic.jpg", "assets/slider/random.jpg", "assets/slider/practice.jpg"];
-  startIndex = 1;
-
-  functionSlide() {
-    const slides = Array.from(document.getElementsByClassName('slider__fade'));
-    if (slides === []) {
-      this.Repeat();
-    }
-    for (const x of slides) {
-      const y = x as HTMLElement;
-      y.style.display = 'none';
-    }
-    if (this.startIndex > slides.length - 1) {
-      this.startIndex = 0;
-      const slide = slides[this.startIndex] as HTMLElement;
-      slide.style.display = 'block';
-      this.startIndex++;
-    } else {
-
-      const slide = slides[this.startIndex] as HTMLElement;
-      slide.style.display = 'block';
-      this.startIndex++;
-    }
-  }
-
-  Repeat() {
-    setTimeout(() => {
-      this.functionSlide();
-      this.Repeat();
-    }, 4000);
-  }
-
   ngOnInit(): void {
-    this.Repeat();
+
   }
 
 }
