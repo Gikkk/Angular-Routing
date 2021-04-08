6a7492a43b4d495b4edafe15340a5b121bca3acb
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-04-08 21:32:11 +0400

    add

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,42 +1,15 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy,Renderer2,ViewChild } from '@angular/core';
-import { FocusComponent } from '../focus/focus.component';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements AfterViewInit, OnDestroy{
+export class AboutComponent implements AfterViewInit {
 
-  constructor(private renderer: Renderer2) { }
-
-  @ViewChild('fadeInRight') fadeInRight: ElementRef;
-  @ViewChild('child') childcomp: FocusComponent;
-
-  options = {
-    rootMargin: '0px',
-    threshold: 0.3
-  };
-  private observer: IntersectionObserver | undefined;
+  constructor() { }
 
   ngAfterViewInit() {
-    this.observer = new IntersectionObserver( entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.renderer.addClass(this.fadeInRight.nativeElement, 'fadeInAnimRight');
-          this.observer.unobserve(entry.target)
-          console.log('projects loaded');
-        }
-      });
-    }, this.options);
 
-    this.observer.observe(this.fadeInRight.nativeElement as HTMLElement);
-  }
-
-  ngOnDestroy(){
-    if (this.observer) {
-      this.observer.disconnect();
-      this.observer = undefined;
-    }
   }
 }
--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,41 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy,Renderer2,ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.scss']
 })
-export class ProjectComponent implements AfterViewInit {
+export class ProjectComponent implements AfterViewInit, OnDestroy {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
+
+  @ViewChild('fadeInAnim') fadeInAnim: ElementRef;
+  @ViewChild('fadeInRight') fadeInRight: ElementRef;
+  @ViewChild('fadeInLeft') fadeInLeft: ElementRef;
+
+  options = {
+    rootMargin: '0px',
+    threshold: 0.1
+  };
+  private observer: IntersectionObserver | undefined;
 
   ngAfterViewInit() {
+    this.observer = new IntersectionObserver( entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(this.fadeInRight.nativeElement, 'fadeInAnimRight');
+          this.observer.unobserve(entry.target)
+        }
+      });
+    }, this.options);
 
+    this.observer.observe(this.fadeInRight.nativeElement as HTMLElement);
+  }
+
+  ngOnDestroy(){
+    if (this.observer) {
+      this.observer.disconnect();
+      this.observer = undefined;
+    }
   }
 }
