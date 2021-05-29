4e6fcb60735153aaff165b9765458f9e64dfe0f0
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-05-29 16:21:25 +0400

    Starting advance angular routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import {  RouterOutlet } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
-import { routeTransitionAnimations } from './shared/helpers/route-transition-animations';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [routeTransitionAnimations]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
-  // route animation
-  prepareRoute(outlet: RouterOutlet) {
-    return outlet &&
-      outlet.activatedRouteData &&
-      outlet.activatedRouteData['animationState'];
-  }
-
-  // multi language
-  constructor(translate: TranslateService) {
-    translate.setDefaultLang('en');
-    translate.use('en');
-  }
-
-  ngOnInit() {}
+export class AppComponent {
+  title = 'Angular-Routing';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule, HttpClient} from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './shared/navigation/navigation.component';
-import { CircularMenuComponent } from './shared/circular-menu/circular-menu.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    CircularMenuComponent,
-    FooterComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    TranslateModule.forRoot({
-      defaultLanguage: 'en',
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-  }),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
