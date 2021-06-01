ab4aa3eb45330201d1dbba6cd276aae4125c45da
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-06-01 23:36:36 +0400

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +19,8 @@ import { BlogComponent } from './blog/blog.component';
     BlogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
