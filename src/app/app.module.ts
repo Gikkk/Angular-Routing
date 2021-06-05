0420a98a9553de42cc80b9bc6d1d9b4eb5ed9d67
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-06-05 23:44:39 +0400

    adding lazy loaded modules - updating soon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { BlogComponent } from './blog/blog.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { TechComponent } from './blog/tech/tech.component';
-import { ProgrammingComponent } from './blog/programming/programming.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent,
-    ProjectsComponent,
-    BlogComponent,
-    PageNotFoundComponent,
-    TechComponent,
-    ProgrammingComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BlogComponent } from './blog.component';
+import { TechComponent } from './tech/tech.component';
+import { ProgrammingComponent } from './programming/programming.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BlogComponent,
+    TechComponent,
+    ProgrammingComponent
+  ],
   imports: [
     CommonModule
   ]
