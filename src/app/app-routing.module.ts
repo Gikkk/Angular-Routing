9421e18cb528448b0bbec55d1a832cc2df6c1524
Gikkk <giorgi.zhonzholadze@tesau.edu.ge>
2021-06-06 23:45:30 +0400

    Adding lazy loaded modules + AOT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { BlogComponent } from './blog/blog.component';
-import { TechComponent } from './blog/tech/tech.component';
-import { ProgrammingComponent } from './blog/programming/programming.component';
-import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  {
-    path: 'blog',
-    component: BlogComponent,
-    children: [
-      {
-        path: 'tech', // child route path
-        component: TechComponent, // child route component that the router renders
-      },
-      {
-        path: 'programming',
-        component: ProgrammingComponent, // another child route component that the router renders
-      },
-    ],
-  },
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent},
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
+  { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)  },
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)  },
   { path: '',   redirectTo: '/', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot( routes, {
+    preloadingStrategy: PreloadAllModules,
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
